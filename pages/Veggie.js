4cd99a45a18a372ab1e8c7abb473b25933af0f39
DoroVernier <doro.vernier@outlook.de>
2022-09-06 17:17:36 +0200

    add locale image

--- pages/Veggie.js
+++ pages/Veggie.js
@@ -1,8 +1,18 @@
+import Image from 'next/image'; 
+import pizza from '../public/pizza.jpg';
+
+
 export default function Dessert() {
   return (
     <>
-      <h2>Desserts</h2>
+      <h2>Dishes</h2>
       <p>Here, you can find recepies perfect ending for a delicious meal</p>
+      <Image 
+      alt=""
+      src={pizza}
+      layout="responsive"
+      width={2400}
+    height={1599} />
     </>
   );
 }
