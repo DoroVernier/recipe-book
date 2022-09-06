8a25d0ed778003d365a60f17be0be72d496cc8f7
DoroVernier <doro.vernier@outlook.de>
2022-09-06 13:00:45 +0200

    initial set-up

---
+++ pages/Dessert.js
@@ -0,0 +1,8 @@
+export default function Dessert() {
+  return (
+    <>
+      <h2>Vegetarian Recipes</h2>
+      <p>Easy, healthy and delicious!</p>
+    </>
+  );
+}
---
+++ pages/Veggie.js
@@ -0,0 +1,8 @@
+export default function Dessert() {
+  return (
+    <>
+      <h2>Desserts</h2>
+      <p>Here, you can find recepies perfect ending for a delicious meal</p>
+    </>
+  );
+}
