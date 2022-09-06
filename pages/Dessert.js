5404ae9f7302f83d599148652e8854b219fd8d84
DoroVernier <doro.vernier@outlook.de>
2022-09-06 18:15:36 +0200

    add dessert image

--- pages/Dessert.js
+++ pages/Dessert.js
@@ -1,8 +1,17 @@
+import Image from "next/image"; 
+
 export default function Dessert() {
   return (
     <>
-      <h2>Vegetarian Recipes</h2>
-      <p>Easy, healthy and delicious!</p>
-    </>
+      <h2>Delicious Desserts</h2>
+      <p>Here, you can find recepies for the perfect ending of an exquisite meal!</p>
+      <Image 
+      alt=""
+      src="https://images.unsplash.com/photo-1469533778471-92a68acc3633?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80"
+      layout="responsive"
+      width="2400"
+      height="1599"
+    />
+      </>
   );
 }
--- pages/Veggie.js
+++ pages/Veggie.js
@@ -2,17 +2,18 @@ import Image from 'next/image';
 import pizza from '../public/pizza.jpg';
 
 
+
 export default function Dessert() {
   return (
     <>
       <h2>Dishes</h2>
-      <p>Here, you can find recepies perfect ending for a delicious meal</p>
+      <p>Easy, healthy and delicious!</p>
       <Image 
       alt=""
       src={pizza}
       layout="responsive"
       width={2400}
-    height={1599} />
+      height={1599} />
     </>
   );
 }
